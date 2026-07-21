b02d2880560bdd4988cb58505bcf2ff7455ea4c0
ankitsinhchauhan <ankitchauhan3020@gmail.com>
2026-07-21 21:01:04 +0530

    Integrated Bootstrap 5

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,18 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+TEMPLATES = [
+    {
+        'DIRS' : [BASE_DIR / "templates"]
+    }
+]
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
