80536929dd62dbb291a1651a67b27fcdf3b2e294
ankitsinhchauhan <ankitchauhan3020@gmail.com>
2026-07-21 17:38:50 +0530

    Created User, EmployeeProfile and Document Models

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,3 +4,11 @@ from .models import User
 # Register your models here.
 
 admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'personal_email', 'company_email', 'role', 'status')
+    list_filter = ('role', 'status')
+    serarch_fields = ('full_name', 'personal_email')
+
+
--- documents/admin.py
+++ documents/admin.py
@@ -4,3 +4,7 @@ from .models import Document
 # Register your models here.
 
 admin.site.register(Document)
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'status', 'uploaded_at')
+    list_filter = ('status')
